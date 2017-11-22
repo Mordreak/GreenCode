dfb7dde2169124ec6d9610c604cfcbf0cdf7287c
Théodore Wittmann <t.wittmann@lesnouveauxateliers.com>
2017-11-22 21:49:36 +0100

    Checking memcached

--- src/GC/MainBundle/Controller/DefaultController.php
+++ src/GC/MainBundle/Controller/DefaultController.php
@@ -8,6 +8,11 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('GCMainBundle:Default:index.html.twig');
+        $working = false;
+        $this->get('memcache.default')->set('someKey', 'someValue', 0, true);
+        $working = $this->get('memcache.default')->get('someKey');
+        return $this->render('GCMainBundle:Default:index.html.twig', array(
+            'working' => $working
+        ));
     }
 }
