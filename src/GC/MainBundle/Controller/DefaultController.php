1958e6635298b210fe061a58401ff00e074962a9
Théodore Wittmann <t.wittmann@lesnouveauxateliers.com>
2017-11-22 21:54:28 +0100

    Changing ttl value in main controller

--- src/GC/MainBundle/Controller/DefaultController.php
+++ src/GC/MainBundle/Controller/DefaultController.php
@@ -9,7 +9,7 @@ class DefaultController extends Controller
     public function indexAction()
     {
         $working = false;
-        $this->get('memcache.default')->set('someKey', 'someValue', 0, true);
+        $this->get('memcache.default')->set('someKey', true, 0, 345600);
         $working = $this->get('memcache.default')->get('someKey');
         return $this->render('GCMainBundle:Default:index.html.twig', array(
             'working' => $working
