df19c39513c15888097becf18a94ea447e474848
Lucas Moisan <lucas@villagemap.fr>
2017-11-23 22:49:15 +0100

    Added a function to calculate the pagination mdr.

--- src/GC/MainBundle/Controller/DefaultController.php
+++ src/GC/MainBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace GC\MainBundle\Controller;
 use GC\MainBundle\Entity\Dentist;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use GC\MainBundle\Repository\DentistRepository;
 
 class DefaultController extends Controller
 {
@@ -58,43 +59,50 @@ class DefaultController extends Controller
 
         $resultsCount = $this->get('memcache.default')->get($query . '-' . $page . '-' . implode(';', $openDays) . '-' . $openHour . '-count');
 
+        $pagination = $this->_getPagination($request->getUri(), intval($request->query->get('p')), $resultsCount);
+
+        return $this->render('GCMainBundle:Default:search.html.twig', compact(
+            'results', 'resultsCount', 'query', 'page', 'pagination'
+        ));
+    }
+
+    protected function _getPagination($currentUri, $currentPage, $resultsCount)
+    {
         $pagination = array();
         $maxPages = 10;
         $pageCount = 0;
         $pageItr = -($maxPages/2);
 
-        if ($page > 1)
+        if ($currentPage > 1)
         {
-            $url = str_replace('p=' . $page, 'p=' . ($page - 1), $request->getUri());
-            $pagination['previous'] = ['number' => $page - 1, 'url' => $url];
+            $url = str_replace('p=' . $currentPage, 'p=' . ($currentPage - 1), $currentUri);
+            $pagination['previous'] = ['number' => $currentPage - 1, 'url' => $url];
         }
-        if ($page < ceil($resultsCount / 20))
+        if ($currentPage < ceil($resultsCount / 20))
         {
-            $url = str_replace('p=' . $page, 'p=' . ($page + 1), $request->getUri());
-            $pagination['next'] = ['number' => $page + 1, 'url' => $url];
+            $url = str_replace('p=' . $currentPage, 'p=' . ($currentPage + 1), $currentUri);
+            $pagination['next'] = ['number' => $currentPage + 1, 'url' => $url];
         }
 
         while ($pageCount <= $maxPages)
         {
-            $pageNumber = $page + $pageItr;
-            if ($pageNumber >  ceil($resultsCount / 20))
+            $pageNumber = $currentPage + $pageItr;
+            if ($pageNumber >  ceil($resultsCount / DentistRepository::RESULTS_PER_PAGE))
                 break;
 
             if ($pageNumber > 0)
             {
-                if ($request->query->get('p'))
-                    $url = str_replace('p=' . $page, 'p=' . $pageNumber, $request->getUri());
+                if ($currentPage)
+                    $url = str_replace('p=' . $currentPage, 'p=' . $pageNumber, $currentUri);
                 else
-                    $url = $request->getUri() . '&p=' . $pageNumber;
+                    $url = $currentUri . '&p=' . $pageNumber;
                 $pagination['pages'][] = ['number' => $pageNumber, 'url' => $url];
                 $pageCount++;
             }
             $pageItr++;
         }
 
-        return $this->render('GCMainBundle:Default:search.html.twig', compact(
-            'results', 'resultsCount', 'query', 'page', 'pagination'
-        ));
+        return $pagination;
     }
 
     /**
