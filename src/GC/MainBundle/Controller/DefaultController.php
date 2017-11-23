30625d1be72e49afc74d14389d931ef933ce609a
Lucas Moisan <lucas@villagemap.fr>
2017-11-23 16:16:50 +0100

    Revert "Fuck memcached."
    This reverts commit 92b2ba71561ef8f23807201e07420ffb20c627cc.

--- src/GC/MainBundle/Controller/DefaultController.php
+++ src/GC/MainBundle/Controller/DefaultController.php
@@ -14,8 +14,8 @@ class DefaultController extends Controller
     public function indexAction()
     {
         $working = false;
-        // $this->get('memcache.default')->set('someKey', true, 0, 345600);
-        // $working = $this->get('memcache.default')->get('someKey');
+        $this->get('memcache.default')->set('someKey', true, 0, 345600);
+        $working = $this->get('memcache.default')->get('someKey');
 
         return $this->render('GCMainBundle:Default:index.html.twig', array(
             'working' => $working
